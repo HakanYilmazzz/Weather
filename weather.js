162a8a118f40043482ae714e0cfe2dfdcc48f779
Hakan YILMAZ <hakanyilmazzz93@gmail.com>
2021-08-11 14:37:07 +0300

    First Commit

---
+++ weather.js
@@ -0,0 +1,65 @@
+const api = {
+  key: "YOUR_API_KEY",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+fetch(`${api.base}weather?q=balıkesir&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+    
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults).catch((err)=>{
+      Swal.fire({
+        title: "",
+        text: "İlgili şehir bulunamadı.",
+        icon: "warning",
+        confirmButtonText: "Tamam",
+      });
+      searchbox.value ="";
+    })
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `Şuanki: ${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let temp2 = document.querySelector('.current .feel');
+  temp2.innerHTML = `Hissedilen: ${Math.round(weather.main.feels_like)}<span>°C</span>`;
+  
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
+function dateBuilder (d) {
+  let months = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"];
+  let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return ` ${date} ${month} ${year}, ${day}`;
+}
